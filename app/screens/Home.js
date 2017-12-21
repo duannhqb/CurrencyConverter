b0626c4f75126f96d05e842b0309063458a1b5e9
amandeepmittal <amanmittal.email@gmail.com>
2017-12-21 19:45:06 +0530

    Home Screen: Button

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -3,6 +3,7 @@ import { StatusBar } from 'react-native';
 import { Container } from '../components/Container';
 import { Logo } from '../components/Logo';
 import { InputWidthButton } from '../components/TextInput/';
+import { ClearButton } from '../components/Buttons';
 
 const TEMP_BASE_CURRENCY = 'USD';
 const TEMP_QUOTE_CURRENCY = 'GBP';
@@ -20,6 +21,10 @@ class Home extends Component {
 
   handleTextChange = (text) => {
     console.log('change text', text);
+  };
+
+  handleSwapCurrency = () => {
+    console.log('press swap currency');
   };
 
   render() {
@@ -41,6 +46,7 @@ class Home extends Component {
           editable={false}
           value={TEMP_QUOTE_PRICE}
         />
+        <ClearButton text="Reverse Currencies" onPress={this.handleSwapCurrency} />
       </Container>
     );
   }
