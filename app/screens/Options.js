6017a4cca5949988a84606ee35628f014c3e34b3
amandeepmittal <amanmittal.email@gmail.com>
2017-12-22 15:33:47 +0530

    Options Screen: External Linking

--- app/screens/Options.js
+++ app/screens/Options.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { ScrollView, StatusBar, Platform } from 'react-native';
+import { ScrollView, StatusBar, Platform, Linking } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import PropTypes from 'proptypes';
 
@@ -18,7 +18,8 @@ class Options extends Component {
   };
 
   handleSitePress = () => {
-    console.log('press site');
+    Linking.openURL('http://fixer.io').catch(() =>
+      alert('An Error occurred when opening the link!'));
   };
 
   render() {
